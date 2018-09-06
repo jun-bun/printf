f33e095382ba35ef82430316c57a88c3e7ba9ded
juwong <juwong@42.US.ORG>
2018-09-05 23:07:21 -0700

    test S

--- sources/parse_string.c
+++ sources/parse_string.c
@@ -6,7 +6,7 @@
 /*   By: juwong <juwong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 23:35:17 by juwong            #+#    #+#             */
-/*   Updated: 2018/09/05 23:05:44 by juwong           ###   ########.fr       */
+/*   Updated: 2018/09/05 23:06:59 by juwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,7 +85,7 @@ void		get_str_wchar(char c, t_arg *arg, va_list ap)
 	}
 	str = va_arg(ap, wchar_t *);
 	arg->format = ft_strdup((char *)str);
-	arg->size = 2;
+	arg->size = 3;
 	format_precision(arg, c);
 	format_padding(arg, c);
 }
