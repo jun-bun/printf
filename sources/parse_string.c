bf4bc792bd6bb9c0fbb981c70e4e81b2136abafd
juwong <juwong@42.US.ORG>
2018-09-05 21:12:59 -0700

    test S again

--- sources/parse_string.c
+++ sources/parse_string.c
@@ -6,7 +6,7 @@
 /*   By: juwong <juwong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 23:35:17 by juwong            #+#    #+#             */
-/*   Updated: 2018/09/05 21:12:23 by juwong           ###   ########.fr       */
+/*   Updated: 2018/09/05 21:12:50 by juwong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,7 @@ void		get_str_wchar(char c, t_arg *arg, va_list ap)
 		str = va_arg(ap, wchar_t *);
 	}
 	arg->format = ft_strdup((char *)str);
-	arg->size = 4;
+	arg->size = 8;
 	format_precision(arg, c);
 	format_padding(arg, c);
 }
